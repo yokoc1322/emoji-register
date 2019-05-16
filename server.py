8eda99de999eadc4cab058d414e478486c846f3d
yokoc1322 <6673423+yokoc1322@users.noreply.github.com>
2019-05-16 22:47:30 +0900

    Support newline

--- server.py
+++ server.py
@@ -12,7 +12,7 @@ def route():
     args = request.form['text'].split()
     if len(args) < 2:
         abort(400)
-    emoji_text = args[0]
+    emoji_text = re.sub('\\n', args[0], '\n')
     emoji_name = args[1]
     name_checker = re.compile(r"^[a-z0-9\-_]+$")
 
@@ -28,5 +28,4 @@ if __name__ == '__main__':
     if not os.environ.get('SLACK_APP_TOKEN'):
         print("Please set SLACK_APP_TOKEN to env val ")
     else:
-        # app.run(host='0.0.0.0', port=80)
-        app.run()
+        app.run(host='0.0.0.0', port=80)
