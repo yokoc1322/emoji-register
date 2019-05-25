5e980f51cd1e4efcc8824a5a401a57490f2dacca
yokoc1322 <6673423+yokoc1322@users.noreply.github.com>
2019-05-25 14:05:39 +0900

    Fix to align center when each line has only single character

--- emoji-register/emoji.py
+++ emoji-register/emoji.py
@@ -2,6 +2,7 @@ from slack_emojinator.upload import upload_main
 import emojilib
 import pathlib
 import random
+from functools import reduce
 
 _DEFAULT_FONT_FILE = 'fonts/NotoSansCJKjp-hinted/NotoSansCJKjp-Bold.otf'
 
@@ -21,13 +22,18 @@ def _create_random_color():
     return '%02X%02X%02XFF' % (red, blue, green)
 
 
+def _is_every_line_1_char(text):
+    is_line_1_char = map(lambda x: len(x) == 1, text.split('\n'))
+    return reduce(lambda x, acc: x and acc, is_line_1_char, True)
+
+
 def generate(
     text,
     filename,
     typeface_file=_DEFAULT_FONT_FILE,
     width=128,
     height=128,
-    align='left',
+    default_align='left',
     color=None,
     background_color=None,
     size_fixed=None,
@@ -42,6 +48,7 @@ def generate(
         generate_options['size_fixed'] = size_fixed
     if disable_stretch:
         generate_options['disable_stretch'] = disable_stretch
+    align = 'center' if _is_every_line_1_char(text) else default_align
 
     data = emojilib.generate(
         text=text,
