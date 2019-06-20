02b9425062962b3fa6fc8a3e21804e27fe1854bb
yokoc1322 <6673423+yokoc1322@users.noreply.github.com>
2019-06-20 23:50:40 +0900

    Fix style by black

--- emoji-register/emoji.py
+++ emoji-register/emoji.py
@@ -4,7 +4,7 @@ import pathlib
 import random
 from functools import reduce
 
-_DEFAULT_FONT_FILE = 'fonts/NotoSansCJKjp-hinted/NotoSansCJKjp-Bold.otf'
+_DEFAULT_FONT_FILE = "fonts/NotoSansCJKjp-hinted/NotoSansCJKjp-Bold.otf"
 
 
 def _create_random_color():
@@ -19,11 +19,11 @@ def _create_random_color():
         blue = random.randint(COLOR_MIN, COLOR_MAX)
         green = random.randint(COLOR_MIN, COLOR_MAX)
         avg = (red + blue + green) / 3
-    return '%02X%02X%02XFF' % (red, blue, green)
+    return "%02X%02X%02XFF" % (red, blue, green)
 
 
 def _is_every_line_1_char(text):
-    is_line_1_char = map(lambda x: len(x) == 1, text.split('\n'))
+    is_line_1_char = map(lambda x: len(x) == 1, text.split("\n"))
     return reduce(lambda x, acc: x and acc, is_line_1_char, True)
 
 
@@ -33,22 +33,22 @@ def generate(
     typeface_file=_DEFAULT_FONT_FILE,
     width=128,
     height=128,
-    default_align='left',
+    default_align="left",
     color=None,
     background_color=None,
     size_fixed=None,
-    disable_stretch=None
+    disable_stretch=None,
 ):
     generate_options = {}
     if color:
-        generate_options['color'] = color
+        generate_options["color"] = color
     if background_color:
-        generate_options['background_color'] = background_color
+        generate_options["background_color"] = background_color
     if size_fixed:
-        generate_options['size_fixed'] = size_fixed
+        generate_options["size_fixed"] = size_fixed
     if disable_stretch:
-        generate_options['disable_stretch'] = disable_stretch
-    align = 'center' if _is_every_line_1_char(text) else default_align
+        generate_options["disable_stretch"] = disable_stretch
+    align = "center" if _is_every_line_1_char(text) else default_align
 
     data = emojilib.generate(
         text=text,
@@ -61,12 +61,12 @@ def generate(
         # format='PNG',  # PING or WEBP
     )
 
-    with open(filename, 'wb') as f:
+    with open(filename, "wb") as f:
         f.write(data)
 
 
 def register_emoji(text, name):
     color = _create_random_color()
-    filename = '/tmp/' + name + '.png'
+    filename = "/tmp/" + name + ".png"
     generate(text, filename, color=color)
     return upload_main(pathlib.Path(filename).resolve())
--- emoji-register/server.py
+++ emoji-register/server.py
@@ -3,17 +3,18 @@ import re
 import sys
 from flask import Flask, request, abort
 from emoji import register_emoji
+
 app = Flask(__name__)
 
 
-@app.route('/', methods=['POST'])
+@app.route("/", methods=["POST"])
 def route():
-    if request.form['token'] != os.environ.get('SLACK_APP_TOKEN'):
+    if request.form["token"] != os.environ.get("SLACK_APP_TOKEN"):
         abort(403)
-    args = request.form['text'].split()
+    args = request.form["text"].split()
     if len(args) < 2:
         abort(400)
-    emoji_text = re.sub('\\n', args[0], '\n')
+    emoji_text = re.sub("\\n", args[0], "\n")
     emoji_name = args[1]
     name_checker = re.compile(r"^[a-z0-9\-_]+$")
 
@@ -23,14 +24,14 @@ def route():
     return register_emoji(emoji_text, emoji_name)
 
 
-if __name__ == '__main__':
-    error_message = ''
-    if not os.environ.get('SLACK_APP_TOKEN'):
+if __name__ == "__main__":
+    error_message = ""
+    if not os.environ.get("SLACK_APP_TOKEN"):
         error_message += "Please set SLACK_APP_TOKEN to env val.\n"
-    if not os.environ.get('PORT'):
+    if not os.environ.get("PORT"):
         error_message += "Please set PORT to env val.\n"
     if len(error_message) > 0:
         print(error_message)
         sys.exit(1)
 
-    app.run(host='0.0.0.0', port=os.environ.get('PORT'))
+    app.run(host="0.0.0.0", port=os.environ.get("PORT"))
