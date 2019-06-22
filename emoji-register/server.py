d598e121093dc7e17a5324cec3d5d40e9bd2f921
yokoc1322 <6673423+yokoc1322@users.noreply.github.com>
2019-06-22 14:51:45 +0900

    Support delayed return

--- emoji-register/server.py
+++ emoji-register/server.py
@@ -1,10 +1,28 @@
 import os
 import re
 import sys
+import json
+import logging
+from multiprocessing import Process
+
+import requests
 from flask import Flask, request, abort
 from emoji import register_emoji
 
+LOGGER_NAME = "emoji_register"
+
 app = Flask(__name__)
+
+
+def _register_emoji_with_delay_return(response_url, emoji_text, emoji_name):
+    return_text = register_emoji(emoji_text, emoji_name)
+    headers = {"content-type": "application/json"}
+    data = {"text": return_text}
+    r = requests.post(response_url, data=json.dumps(data), headers=headers)
+
+    logger = logging.getLogger(LOGGER_NAME + ".register")
+    logger.info(r.status_code)
+    logger.info(r.text)
 
 
 @app.route("/", methods=["POST"])
@@ -21,17 +39,27 @@ def route():
     if not name_checker.match(emoji_name):
         abort(400)
 
-    return register_emoji(emoji_text, emoji_name)
+    response_url = request.form["response_url"]
+    p = Process(
+        target=_register_emoji_with_delay_return,
+        args=(response_url, emoji_text, emoji_name),
+    )
+    p.start()
+    return "Registering %s ...." % emoji_name
 
 
 if __name__ == "__main__":
-    error_message = ""
+    logging.basicConfig()
+    logger = logging.getLogger(LOGGER_NAME)
+
+    error_messages = []
     if not os.environ.get("SLACK_APP_TOKEN"):
-        error_message += "Please set SLACK_APP_TOKEN to env val.\n"
+        error_messages.append("Please set SLACK_APP_TOKEN to env val.")
     if not os.environ.get("PORT"):
-        error_message += "Please set PORT to env val.\n"
-    if len(error_message) > 0:
-        print(error_message)
+        error_messages.append("Please set PORT to env val.")
+    if len(error_messages) > 0:
+        for message in error_messages:
+            logger.error(message)
         sys.exit(1)
 
     app.run(host="0.0.0.0", port=os.environ.get("PORT"))
