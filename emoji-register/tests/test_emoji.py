7475d04eab4c1e166740a1ea902a94481ce973a1
yokoc1322 <6673423+yokoc1322@users.noreply.github.com>
2019-06-23 14:31:15 +0900

    Add test for emoji

--- emoji-register/tests/test_emoji.py
+++ emoji-register/tests/test_emoji.py
@@ -1,3 +1,39 @@
-# url = "https://emojis.slackmojis.com/emojis/images/1469223471/679/charmander_dancing.gif?1469223471"
-# emoji_text = "麗"
-# name = "hitokage"
+import os
+import sys
+import re
+from pathlib import Path
+import pytest
+
+sys.path.append(str(Path(__file__).parents[1] / "src"))
+
+import emoji
+
+IMAGE_FILE_NAME = "image"
+IMAGE_FILE_DIR = "/tmp"
+IMAGE_FILE_PATH = "/tmp/image.png"
+
+
+@pytest.fixture
+def delete_image_file():
+    if Path(IMAGE_FILE_PATH).exists():
+        os.remove(IMAGE_FILE_PATH)
+    yield
+    if Path(IMAGE_FILE_PATH).exists():
+        os.remove(IMAGE_FILE_PATH)
+
+
+def test_create_random_color():
+    color_code = emoji.create_random_color()
+    assert re.match(r"[0-9A-F]{6}FF", color_code) is not None
+
+
+def test_generate_moji(delete_image_file):
+    text = "麗"
+    emoji.generate_moji(text, IMAGE_FILE_PATH)
+    assert Path(IMAGE_FILE_PATH).exists()
+
+
+def test_download_image(delete_image_file):
+    url = "https://emojis.slackmojis.com/emojis/images/1450319441/51/facepalm.png?1450319441"
+    emoji.download_image(url, IMAGE_FILE_DIR, IMAGE_FILE_NAME)
+    assert Path(IMAGE_FILE_PATH).exists()
